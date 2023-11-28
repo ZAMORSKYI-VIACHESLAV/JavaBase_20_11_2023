774545dfefb4460902c35d1075e8f7b5c94f3361
ZAMORSKYI VIACHESLAV <120983476s@gmail.com>
2023-11-28 14:06:09 +0100

    test2

---
+++ src/main/java/ua/hillel/zamorskiy/lessons5/Main.java
@@ -0,0 +1,115 @@
+package ua.hillel.zamorskiy.lessons5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название команды");
+        String intPlayer=(scanner.nextLine());
+
+int intPlayer1=0;
+        System.out.println("Введите значение для первого игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer1= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer2=0;
+        System.out.println("Введите значение для первого игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer2= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer3=0;
+        System.out.println("Введите значение для первого игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer3= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer4=0;
+        System.out.println("Введите значение для первого игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer4= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer5=0;
+        System.out.println("Введите значение для первого игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer5= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int team1 =((intPlayer1+intPlayer2+intPlayer4+intPlayer3+intPlayer5)/5);
+        System.out.println(team1);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/ua/hillel/zamorskiy/test/test.java
@@ -0,0 +1,10 @@
+package ua.hillel.zamorskiy.test;
+
+public class test {
+    public static void main(String[] args) {
+ //     System.out.println(3420-94);
+
+
+
+    }
+}
