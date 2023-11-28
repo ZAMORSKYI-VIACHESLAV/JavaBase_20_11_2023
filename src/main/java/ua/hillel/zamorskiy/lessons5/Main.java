3856c5973bd33f44341999c3f5aaf9a1108d147e
ZAMORSKYI VIACHESLAV <120983476s@gmail.com>
2023-11-28 14:06:09 +0100

    test2

--- src/main/java/ua/hillel/zamorskiy/lessons5/Main.java
+++ src/main/java/ua/hillel/zamorskiy/lessons5/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название команды");
+        System.out.println("Введите название 1 команды");
         String intPlayer=(scanner.nextLine());
 
 int intPlayer1=0;
@@ -61,6 +61,82 @@ int intPlayer1=0;
         }
         int team1 =((intPlayer1+intPlayer2+intPlayer4+intPlayer3+intPlayer5)/5);
         System.out.println(team1);
+
+
+
+
+
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.println("Введите название 2 команды");
+        String Player =(scanner1.nextLine());
+
+
+
+        int intPlayer1a1=0;
+        System.out.println("Введите значение для 1 игрока ");
+
+
+        if (scanner.hasNextInt()) {
+            intPlayer1a1= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer2a=0;
+        System.out.println("Введите значение для 2 игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer2a= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer3a=0;
+        System.out.println("Введите значение для 3  игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer3a= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer4a=0;
+        System.out.println("Введите значение для 4 игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer4a= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int intPlayer5a=0;
+        System.out.println("Введите значение для 5 игрока ");
+
+        if (scanner.hasNextInt()) {
+            intPlayer5a= scanner.nextInt();
+        }
+        else {
+            System.out.println("Неверно , перезапусти");
+            System.exit(0);
+        }
+        int team2 =((intPlayer1a1+intPlayer2a+intPlayer4a+intPlayer3a+intPlayer5a)/5);
+        System.out.println(team2);
+
+
+
+            if (team1 < team2)
+            {
+                 System.out.println("Одержала победу команда" + team2+ Player);
+            }
+            if (team1 > team2) {
+                System.out.println("Одержала победу команда" + team1+ intPlayer);
+            }
         }
 
 
@@ -69,6 +145,7 @@ int intPlayer1=0;
 
 
 
+    }
 
 
 
@@ -100,7 +177,14 @@ int intPlayer1=0;
 
 
 
-}
+
+
+
+
+
+
+
+
 
 
 
