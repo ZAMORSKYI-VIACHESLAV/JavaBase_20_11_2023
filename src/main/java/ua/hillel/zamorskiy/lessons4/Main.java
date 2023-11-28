6b92a8526fe023790f4d24c07dbdf327711d7715
ZAMORSKYI VIACHESLAV <120983476s@gmail.com>
2023-11-28 14:06:09 +0100

    test2

--- src/main/java/ua/hillel/zamorskiy/lessons4/Main.java
+++ src/main/java/ua/hillel/zamorskiy/lessons4/Main.java
@@ -2,8 +2,16 @@ package ua.hillel.zamorskiy.lessons4;
 
 public class Main {
     public static void main(String[] args) {
-        int vork = (18);
-        System.out.println(vork);
+
+        int a = (5);
+        int b = (7);
+        int c = (9);
+        System.out.println("Summa = "+(a+b+c)*4);
+        System.out.println("Volume = "+(a*b*c) + "м³"    );
+
+
+
     }
 
 }
+
---
+++ src/main/java/ua/hillel/zamorskiy/lessons4/NM4.java
@@ -0,0 +1,20 @@
+package ua.hillel.zamorskiy.lessons4;
+
+public class NM4 {
+    public static void main(String[] args) {
+
+        int liWarrion = (14);
+        int liArcher = (24);
+        int liRider = (46);
+        int liAttak = ((liWarrion+liArcher+liRider) *860 );
+
+        System.out.println(liAttak);
+
+        int minWarrion = (9);
+        int minArcher = (35);
+        int minRider = (12);
+        int minAttak = ((minWarrion+minArcher+minRider)*1290 );
+
+        System.out.println(minAttak);
+    }
+}
