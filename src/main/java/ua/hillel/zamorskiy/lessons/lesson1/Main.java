627d2dc9bb53a84e58e2cb28da87e7c9c4189587
ZAMORSKYI VIACHESLAV <120983476s@gmail.com>
2023-11-28 14:06:09 +0100

    add all files

---
+++ src/main/java/ua/hillel/zamorskiy/lessons/lesson1/Main.java
@@ -0,0 +1,7 @@
+package ua.hillel.zamorskiy.lessons.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/ua/hillel/zamorskiy/lessons/lesson3.java
@@ -0,0 +1,4 @@
+package ua.hillel.zamorskiy.lessons;
+
+public class lesson3 {
+}
